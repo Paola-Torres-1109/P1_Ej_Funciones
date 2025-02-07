0a3ec6a9b52b2fc887eec72251b56c954f895e8b
Paola-Torres-1109 <164534880+Paola-Torres-1109@users.noreply.github.com>
2025-02-07 16:47:43 +0000

    mi primera vex

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+//funcion que no devuelve nada
+//funcion saludar
+void saludar(){
+  print("Hola mundo");
+}
+
+void escribeNombre(){
+  print("Paola Torres Valle");
+}
+
+void main(){
+  //Paola Torres Valle Mat: 22308051281109
+  //llamar a la funcion saludar
+  print("Paola Torres Valle Mat: 22308051281109");
+  print("Llamando a la funcion saludar");
+    saludar();
+    print("Llamando a la funcion escribeNombre");
+    escribeNombre();
+}
