37be46c5145ca0079a7014b6925fa4968701e93a
Paola-Torres-1109 <164534880+Paola-Torres-1109@users.noreply.github.com>
2025-02-10 18:05:08 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+//funciones creadas por el usuario
+void suma (int a, int b){
+  print("La suma de $a + $b es: ${a+b}");
+}
+
+void resta (int a, int b){
+  print("La resta de $a - $b es: ${a-b}");
+}
+//funcion main 
+void main(){
+  print("Paola Torres Valle Mat: 22308051281109");
+  print("Llamando a la funcion suma");
+  suma(2,3);
+  resta(5,2);
+}
